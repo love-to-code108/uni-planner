2f866585ce6b12aa898ad4e97e0d57d94bbd87cd
love-to-code108 <love.to.code108@gmail.com>
2024-11-18 23:52:37 +0530

    i am doing something really dangerous right now i hope it works

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,17 @@
-import "./index.scss"
-import { Button } from "@/components/ui/button"
-
-
-
+import "./index.scss";
+import { Routes, Route } from "react-router-dom";
+import { LandingPage } from "./Pages/LandingPage";
+import { LoginPage } from "./Pages/LoginPage";
 
 function App() {
-  
-
   return (
-    <div className=" text-4xl">
-      <Button>Click me</Button>
-    </div>
-  )
+    <Routes>
+      <Route path="/" element={<LandingPage/>}/>
+      <Route path="/login" element={<LoginPage/>}/>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
+
+// 3pm jodi dekhate jai tale amader project 90% ready hoe jabe
