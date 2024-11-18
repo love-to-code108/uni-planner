76c6685c7d161159b58408ca9a68fb549eb82d24
love-to-code108 <love.to.code108@gmail.com>
2024-11-18 16:34:46 +0530

    basic stuff has been done now getting into the real stuff

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import "./index.scss"
+import { Button } from "@/components/ui/button"
 
 function App() {
   
 
   return (
     <div className=" text-4xl">
-      Hello world
+      <Button>Click me</Button>
     </div>
   )
 }
